2aa4a3f03147f739a8e18c341d4248df17d38215
AnoyRC <anoyroyc3545@gmail.com>
2022-03-01 01:01:02 +0530

    First Scroll Trigger Commit

---
+++ Index_JS.js
@@ -0,0 +1,18 @@
+gsap.registerPlugin(ScrollTrigger);
+const sections = gsap.utils.toArray('.slide');
+const numSections = sections.length - 1;
+const snapVal = 1 / numSections;
+
+gsap.to(sections, {
+      xPercent: -100 * numSections,
+      ease: 'none',
+      // x: () => -(document.querySelector('.container').scrollWidth - document.documentElement.clientWidth) + "px",
+      scrollTrigger: {
+        pin:".Parent",
+        scrub: true,
+        snap:snapVal,
+        start: 'top top',
+        end: '+=' + document.querySelector('.container').scrollWidth,
+        markers: true,
+      },
+    });
